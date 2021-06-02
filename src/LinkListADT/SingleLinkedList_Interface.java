4d6e7b9cfe1dbf52e4d82b15efe1c0032d54cef4
Rishabh Gupta <rishabhgupta2498@gmail.com>
2021-06-02 23:55:03 +0530

    updated

--- src/LinkListADT/SingleLinkedList_Interface.java
+++ src/LinkListADT/SingleLinkedList_Interface.java
@@ -1,7 +1,8 @@
 package LinkListADT;
 
 public interface SingleLinkedList_Interface<E> {
-
+    
+    // single linked-list
     void insertNode(E data);
     E removeFromLastPos();
     int searchNodeInLinkedList(E data);
